92449334c718d199c7bf59cc8297169e0d347cc7
kyw7 <kyw7641364175>
2017-12-20 19:53:06 +0800

    2017/12/20 作业报告中加作业记录 添加页面task和taskdetail

---
+++ myapp/src/pages/task/task.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { App,IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http,Response} from "@angular/http";
+import {TaskDetailPage} from "../task-detail/task-detail";
+
+/**
+ * Generated class for the TaskPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-task',
+  templateUrl: 'task.html',
+})
+export class TaskPage {
+
+  // 接收数据
+  listData: Object;
+  // 课程
+  subject: string ="1";
+  user: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private  http: Http,public app:App) {
+
+    this.user = this.navParams.get('item').uid;
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomeworkPage');
+    this.http.request('http://101.201.238.157/index/request_homework_list/'+ this.user+"/"+this.subject)
+      .subscribe((res: Response) => {
+        this.listData = res.json();
+        console.log(this.listData)
+      });
+  }
+
+  //请求不同科目的知识点
+  segmentChanged() {
+    this.http.request('http://101.201.238.157/index/request_homework_list/'+ this.user+"/"+this.subject)
+      .subscribe((res: Response) => {
+        this.listData = res.json();
+      });
+  }
+
+  totaskDetail(hid,name,uid){
+    this.app.getRootNav().push(TaskDetailPage,{"hid":hid,"subject":name,"uid":uid});
+  }
+
+}
