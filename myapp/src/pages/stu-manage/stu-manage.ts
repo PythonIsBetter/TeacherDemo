753535ec0b151a03070928200d843dcc8f83e3b8
kyw7 <641364175@qq.com>
2017-12-26 16:20:40 +0800

    g

--- myapp/src/pages/stu-manage/stu-manage.ts
+++ myapp/src/pages/stu-manage/stu-manage.ts
@@ -4,8 +4,9 @@ import {Http,Response}from "@angular/http";
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import {AddStuPage} from "../add-stu/add-stu";
-import {StuConPage} from "../stu-con/stu-con";
 import {ExercisePage} from "../exercise/exercise";
+import {RecordingvideoPage} from "../recordingvideo/recordingvideo";
+import {}
 
 /**
  * Generated class for the StuManagePage page.
@@ -21,6 +22,8 @@ import {ExercisePage} from "../exercise/exercise";
 })
 export class StuManagePage {
 
+  choose: string ="1";//初始化为0，根据前台的ngmodel绑定值
+
   name:string;
   age:string;
   sex:string;
@@ -31,6 +34,7 @@ export class StuManagePage {
   students:Array<{id:string,name:String,nickname:string,email:string,birthday:string,gender:string,avatar:string,uid:string,school:string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams,public http :Http) {
     this.selectedItem = navParams.get('item');//主要用班级id
+    this.choose = "1";
 
   //  this.url="http://localhost:8090/public/admin/index/getStuPassed";
     this.url="http://101.201.238.157/demo/index/getStuPassed1?id=";
@@ -59,6 +63,19 @@ export class StuManagePage {
     console.log('ionViewDidLoad StuManagePage');
     console.log(this.students);
   }
+
+  /*segmentChanged(item1,item2) {
+    if(this.choose == "2"){
+      this.navCtrl.push(ExercisePage, {
+        item: item2,//student
+      });
+    }
+    if(this.choose == "3"){
+      this.navCtrl.push(RecordingvideoPage, {
+        item: item2,//student
+      });
+    }
+  }*/
 
   itemTapped(event, item) {
     this.navCtrl.push(AddStuPage, {
