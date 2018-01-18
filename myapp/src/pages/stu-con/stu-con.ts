8528d2f55c1cb4e8cb324cc4fb433d4ff574fdb7
jcboy111 <786197140@qq.com>
2018-01-18 15:32:40 +0800

    aaa

--- myapp/src/pages/stu-con/stu-con.ts
+++ myapp/src/pages/stu-con/stu-con.ts
@@ -19,7 +19,10 @@ declare var echarts;
 })
 export class StuConPage {
   @ViewChild('container1') container:ElementRef;//与html中div #container1对应
-
+  @ViewChild('container2') container1:ElementRef;//与html中div #container1对应
+  @ViewChild('container3') container2:ElementRef;//与html中div #container1对应
+  @ViewChild('container4') container3:ElementRef;//与html中div #container1对应
+  @ViewChild('container5') container4:ElementRef;//与html中div #container1对应
   classes:Array<{id:String,name:String,subject:String,head:String}>;
   classid:string;
   chart :any;
@@ -29,50 +32,44 @@ export class StuConPage {
   chart4:any;
   student:any;
   num:number;
-
-
+  // test1:string;
+  // test2:string;
+  // exam1:string;
+  // exam2:string;
+  // exam3:string;
   tests:Array<{id:string,name:string,num:string}>;
-  homeworkDetail:Array<{right:string,wrong:string,all:string}>;
   urlGetHomeworkList;string;
   homework:Array<{id:string,name:string}>;
-  practiceDetail:Array<{right:string,wrong:string,all:string}>;
-  practice:Array<{name:string}>;
+  homeworkMark:Array<{right:string,wrong:string,all:string}>
   urlGetStuPracticeMark:string;
   urlGetStuMathPracticeMark:string;
-  urlGetHomeworkMarkByClassid: string;
- // test:string;
+  urlGetStuHomeworkMark:string;
+  urlGetStuHomeworkMark1:string;
+ test:string;
   constructor(public navCtrl: NavController, public navParams: NavParams,public http :Http) {
     this.tests=[];
+
     this.homework=[];
-    this.homeworkDetail=[];
-    this.practiceDetail=[];
-    this.practice=[];
-    this.classes=[];
-
-    this.classid=navParams.get('item2').id;
     this.student = navParams.get('item');
-
+    this.classes=[];
+    this.classid=navParams.get('item2').id;
+    this.urlGetStuPracticeMark="http://101.201.238.157/demo/index/getStuPracticeMark";
     this.urlGetHomeworkList="http://101.201.238.157/demo/index/getHomeworkList";
+    this.urlGetStuHomeworkMark="http://101.201.238.157/demo/index/getStuHomeworkMark"
     this.urlGetHomeworkList+="?classid="+this.classid;
-
-    this.urlGetHomeworkMarkByClassid = "http://101.201.238.157/demo/index/getHomeworkMarkByClassid";
-    this.urlGetHomeworkMarkByClassid += "?stuid=" + this.student.id + "&classid=" +this.classid;
-
-
+    this.urlGetStuMathPracticeMark=this.urlGetStuPracticeMark+"?stuid="+this.student.id+"&subid="+"2";
+    this.urlGetStuHomeworkMark=this.urlGetStuHomeworkMark+"?stuid="+this.student.id;
     //this.url="http://localhost:8090/public/admin/index/insert";
     //this.url="http://101.201.238.157/demo/index/cla_insert";
     // this.homework.push({
     //   id:"123",
     //   name:"lalala"
     // });
-    this.practice.push({
-      name:"语文"
-    },{
-      name:"数学"
+    this.http.request(this.urlGetStuMathPracticeMark)
+      .subscribe((res:Response)=>{
+       this.test=res.json().data.right;
 
-    },{
-      name:"英语"
-    })
+      });
 
     this.http.request(this.urlGetHomeworkList)
       .subscribe((res:Response)=>{
@@ -82,39 +79,21 @@ export class StuConPage {
             id:res.json().data[i].id,
             name:res.json().data[i].name
           });
+         // this.urlGetStuHomeworkMark1=this.urlGetStuHomeworkMark+"&homeid="+this.homework[i].id;
+         //  this.http.request(this.urlGetStuHomeworkMark1)
+         //    .subscribe((res:Response)=>{
+         //     this.test.push(res.json().data.right);
+         //
+         //    });
         }
         this.num=res.json().data.length;
+       // alert(this.urlGetStuHomeworkMark);
       });
-
-      this.http.request(this.urlGetHomeworkMarkByClassid)
-        .subscribe((res:Response)=>{
-          for(let i=0;i<res.json().data.length;i++)
-          {
-            this.homeworkDetail.push({
-              right:res.json().data[i].right,
-              wrong:res.json().data[i].wrong,
-              all:res.json().data[i].all
-            });
-          }
-        });
-
-      for (let j=1;j<4;j++) {
-        this.urlGetStuPracticeMark = "http://101.201.238.157/demo/index/getStuPracticeMark";
-        this.urlGetStuPracticeMark += "?stuid=" + this.student.id + "&subid="+j;
-        this.http.request(this.urlGetStuPracticeMark)
-          .subscribe((res: Response) => {
-            for (let i = 0; i < 1; i++)
-            {
-              this.practiceDetail.push({
-                right:res.json().data.right,
-                wrong:res.json().data.wrong,
-                all:res.json().data.all
-              });
-            }
-          });
-      }
-
-
+    // this.test1="75";
+    // this.test2="50";
+    // this.exam1="100";
+    // this.exam2="75";
+    // this.exam3="50";
     this.tests.push({
       id:"1",
       name:"语文专项练习",
@@ -167,17 +146,25 @@ export class StuConPage {
   }
 
   ionViewDidLoad() {
+    // alert(this.test);
 
   }
 
+  ionViewWillEnter(){
+    // alert(this.test);
+  }
+
   ionViewDidEnter(){
+ //  alert(this.homework[0].id);
+   //  alert(this.test[0]);
     console.log('ionViewDidLoad StuConPage');
-    console.log(this.homeworkDetail[0]);
+    // alert(this.num);
     for(let i=0;i<this.num;i++){
       var t="chart"+i;
       this.chart = echarts.init(document.getElementById(t));
       this.chart.setOption(
         {
+          clockWise:false,
           tooltip: {
             trigger: 'item',
             formatter: "{a} <br/>{b}: {c} ({d}%)"
@@ -185,15 +172,14 @@ export class StuConPage {
 
           title: {
             floating:true,
-            text:
-            parseInt(this.homeworkDetail[i].right)/parseInt(this.homeworkDetail[i].all)*100+"%",
+            text: parseInt(this.tests[1].num)+"%",
             left:'center',
             top:'center',
-            fontSize:5
+            fontSize:6
           },
           series: [
             {
-              name: '详细情况',
+              name: '访问来源',
               type: 'pie',
               radius: ['50%', '70%'],
               avoidLabelOverlap: false,
@@ -216,72 +202,271 @@ export class StuConPage {
                 }
               },
               data: [
-                {value: parseInt(this.homeworkDetail[i].right), name: '正确'},
-                {value: parseInt(this.homeworkDetail[i].wrong), name: '错误'},
-                {value: 100-parseInt(this.homeworkDetail[i].wrong)-parseInt(this.homeworkDetail[i].right), name: '未做'}
+                {value: parseInt(this.tests[1].num), name: ''},
+                {value: 100-parseInt(this.tests[1].num), name: ''}
               ],
+              color:['#7EC0EE', '#D0D0D0','yellow','blueviolet']
+
             }
-          ],
-          color: ['#3CB371','#DC143C','#c4ccd3','#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83']
+          ]
         }
       );
     }
 
-    for(let i=0;i<3;i++){
-      var r="pie"+i;
-      this.chart = echarts.init(document.getElementById(r));
-      this.chart.setOption(
-        {
-          tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b}: {c} ({d}%)"
-          },
+    let ctx = this.container.nativeElement;
+    this.chart = echarts.init(ctx);
+    this.chart.setOption(
+      {
+        clockWise:false,
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
 
-          title: {
-            floating:true,
-            text: parseInt(this.practiceDetail[i].right)/parseInt(this.practiceDetail[i].all)*100+"%",
-            left:'center',
-            top:'center',
-            fontSize:5
-          },
-          series: [
-            {
-              name: '详细情况',
-              type: 'pie',
-              radius: ['50%', '70%'],
-              avoidLabelOverlap: false,
-              label: {
-                normal: {
-                  show: false,
-                  position: 'center'
-                },
-                emphasis: {
-                  show: true,
-                  textStyle: {
-                    fontSize: '30',
-                    fontWeight: 'bold'
-                  }
-                }
+        title: {
+          floating:true,
+          text: parseInt(this.tests[1].num)+"%",
+          left:'center',
+          top:'center',
+          fontSize:6
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
               },
-              labelLine: {
-                normal: {
-                  show: false
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
                 }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              {value: parseInt(this.tests[1].num), name: ''},
+              {value: 100-parseInt(this.tests[1].num), name: ''}
+            ],
+            color:['#7EC0EE', '#D0D0D0','yellow','blueviolet']
+
+          }
+        ]
+      }
+    );
+    let ctx1 = this.container1.nativeElement;
+    this.chart1 = echarts.init(ctx1);
+    this.chart1.setOption(
+      {
+        clockWise:false,
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+
+        title: {
+          floating:true,
+          text: parseInt(this.tests[0].num)+"%",
+          left:'center',
+          top:'center',
+          fontSize:6
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
               },
-              data: [
-                {value: parseInt(this.practiceDetail[i].right), name: '正确'},
-                {value: parseInt(this.practiceDetail[i].wrong), name: '错误'},
-                {value: 100-parseInt(this.practiceDetail[i].wrong)-parseInt(this.practiceDetail[i].right), name: '未做'}
-              ],
-            }
-          ],
-          color: ['#3CB371','#DC143C','#c4ccd3','#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83']
-        }
-      );
-    }
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              {value: parseInt(this.tests[0].num), name: ''},
+              {value: 100-parseInt(this.tests[0].num), name: ''}
+            ],
+            color:['#7EC0EE', '#D0D0D0','yellow','blueviolet']
 
+          }
+        ]
+      }
+    );
+    let ctx2 = this.container2.nativeElement;
+    this.chart2 = echarts.init(ctx2);
+    this.chart2.setOption(
+      {
+        clockWise:false,
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
 
+        title: {
+          floating:true,
+          text: parseInt(this.tests[2].num)+"%",
+          left:'center',
+          top:'center',
+          fontSize:6
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              {value: parseInt(this.tests[2].num), name: ''},
+              {value: 100-parseInt(this.tests[2].num), name: ''}
+            ],
+            color:['#FFD306', '#D0D0D0','yellow','blueviolet']
 
+          }
+        ]
+      }
+    );
+    let ctx3 = this.container3.nativeElement;
+    this.chart3 = echarts.init(ctx3);
+    this.chart3.setOption(
+      {
+        clockWise:false,
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
 
+        title: {
+          floating:true,
+          text: parseInt(this.tests[3].num)+"%",
+          left:'center',
+          top:'center',
+          fontSize:6
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              {value: parseInt(this.tests[3].num), name: ''},
+              {value: 100-parseInt(this.tests[3].num), name: ''}
+            ],
+            color:['#FFD306', '#D0D0D0','yellow','blueviolet']
+
+          }
+        ]
+      }
+    );
+    let ctx4 = this.container4.nativeElement;
+    this.chart4 = echarts.init(ctx4);
+    this.chart4.setOption(
+      {
+        clockWise:false,
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+
+        title: {
+          floating:true,
+          text: parseInt(this.tests[4].num)+"%",
+          left:'center',
+          top:'center',
+          fontSize:6
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              {value: parseInt(this.tests[4].num), name: ''},
+              {value: 100-parseInt(this.tests[4].num), name: ''}
+            ],
+            color:['#FFD306', '#D0D0D0','yellow','blueviolet']
+
+          }
+        ]
+      }
+    );
   }
 }
